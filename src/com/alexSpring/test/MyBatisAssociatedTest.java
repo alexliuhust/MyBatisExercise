02f3129b63d126a7af1de7eb9a6ca6d05fc2b1d8
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-10-23 23:52:46 -0400

    Mybatis Associated NestedQuery and NestedResult

---
+++ src/com/alexSpring/test/MyBatisAssociatedTest.java
@@ -0,0 +1,64 @@
+package com.alexSpring.test;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import com.alexSpring.po.Person;
+import com.alexSpring.utils.MyBatisUtils;
+
+/**
+ * Beginner Program Test Class
+ * 
+ * @author liu.zehu
+ *
+ */
+public class MyBatisAssociatedTest {
+	
+	/**
+	 * Nested Query
+	 */
+	@Test
+	public void findPersonByIdTest() {
+		SqlSession session = MyBatisUtils.getSession();
+		Person person = session.selectOne("com.alexSpring.mapper.PersonMapper.findPersonById", 1);
+		System.out.println(person);
+		session.close();
+	}
+	
+	/**
+	 * Nested Result
+	 */
+	@Test
+	public void findPersonById_2Test() {
+		SqlSession session = MyBatisUtils.getSession();
+		Person person = session.selectOne("com.alexSpring.mapper.PersonMapper.findPersonById_2", 2);
+		System.out.println(person);
+		session.close();
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+//	public static void main(String[] args) {
+//	}
+}
